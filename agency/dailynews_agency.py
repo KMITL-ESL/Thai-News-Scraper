7f6715ae9494760dcfbd9284cca7c09cef93ec88
TK_DN <63010467@kmitl.ac.th>
2021-07-13 03:33:36 +0700

    update dailynews

--- agency/dailynews_agency.py
+++ agency/dailynews_agency.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from os import replace
 import uuid
 from datetime import datetime, timedelta
 from typing import List
@@ -21,9 +22,12 @@ class DailynewsAgency(Agency):
     def parse_date(self, date_text) -> datetime:
 
         # Trim date name and น.
-        date_text = ' '.join(date_text.strip().split(' ')[1:-1])
-        date_text = date_text.replace('เวลา', '')  # remove เวลา
-        date_text = date_text.replace('  ', ' ')
+        #logging.info(date_text)
+        date_text = ' '.join(date_text.strip().split(' ')[:-1])
+        date_text = date_text.replace('น.', '')  # remove เวลา
+        date_text = date_text.replace(':', '.')
+        date_text = date_text.replace('\n', '')
+        date_text = date_text.replace('\t', '')
         _, thai_month, thai_year, *_ = date_text.split(' ')
         date_text = date_text.replace(
             thai_month, constants.TH_FULL_MONTHS_MAPPER[thai_month])
@@ -32,34 +36,37 @@ class DailynewsAgency(Agency):
         return date
 
     async def scrap_links(self, index_url, from_date, to_date, max_news):
-        root_url = urlparse(index_url).hostname
-        topic = index_url.split('/')[-1]
+        #root_url = urlparse(index_url).hostname
+        #topic = index_url.split('/')[-1]
 
         all_links = set()
         for page_number in range(1, (max_news//constants.DAILYNEWS_MAX_NUM_PER_PAGE)+1):
 
-            soup = await self.scrap_html(index_url, params={'page': page_number})
+            soup = await self.scrap_html(index_url+'page/'+str(page_number), params={'page': page_number})
             if soup is None:
                 logging.error(
                     f'failed to obtain {index_url} with page {page_number}')
                 continue
-
+            
             logging.info(f'page {page_number}')
-            articles = soup.find_all('a', attrs={'class': 'media'}, href=True)
-            # filter article only the article that contains date
-            articles = list(filter(lambda article: article.find(
-                'span', attrs={'class': 'media-date'}) is not None, articles))
-
-            date_texts = list(map(lambda article: article.find(
-                'span', attrs={'class': 'media-date'}).text, articles))
+            
+            articles = soup.find_all('a', attrs={'class': 'elementor-post__thumbnail__link'}, href=True)
+            # *filter article only the article that contains date*
+            #articles = list(filter(lambda article: article.find(
+                #'span', attrs={'class': 'elementor-post__thumbnail__link'}) is not None, articles))
+            date_texts = soup.find_all('div', attrs={'class': 'elementor-post__meta-data'})
+            date_texts = list(map(lambda date_text: date_text.find(
+                'span', attrs={'class': 'elementor-post-date'}).text+' '+
+                date_text.find('span', attrs={'class': 'elementor-post-time'}).text, date_texts))
             dates = list(
                 map(lambda date_text: self.parse_date(date_text), date_texts))
-
+            
             min_date = min(dates)
             max_date = max(dates)
 
             links = list(
-                map(lambda link: f'https://{root_url}{link["href"]}', articles))
+                map(lambda link: f'{link["href"]}', articles))
+            logging.info(articles)
             for date, link in zip(dates, links):
                 all_links.add(link)
                 logging.info(link)
@@ -76,12 +83,16 @@ class DailynewsAgency(Agency):
 
         logging.info(f'scrap {url}')
 
-        title = soup.find('h1', attrs={'class': 'title'}).text.strip()
-        date_text = soup.find('span', attrs={'class': 'date'}).text.strip()
+        title = soup.find('h1', attrs={'class': 'elementor-heading-title elementor-size-default'}).text.strip()
+        date_text = soup.find('span', 
+                    attrs={'class': 'elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-date'}).text.strip()+' '+soup.find('span', 
+                    attrs={'class': 'elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-time'}).text.strip()
         date = self.parse_date(date_text)
         logging.info(date)
-        content = soup.find('div', attrs={'class': 'content-all'}).text.strip()
-        tags = soup.find('ol', attrs={'class': 'breadcrumb'}).find_all('li')
+        content = soup.find('div', 
+                  attrs={'class': 'elementor-element elementor-element-31c4a6f post-content elementor-widget elementor-widget-theme-post-content'}).text.strip().replace('\n', '')
+        tags = soup.find('span', attrs={'class': 'elementor-post-info__terms-list'}).find_all('a', 
+               attrs={'class': 'elementor-post-info__terms-list-item'})
         category = tags[-1].text.strip()
         return RawNewsEntity(publish_date=date,
                              title=title,
