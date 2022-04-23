b0014e73473a2c1cb00c653a9278dc8da38b57d3
TK_DN <63010467@kmitl.ac.th>
2022-04-23 16:24:56 +0700

    add trigger scrap_posttoday

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 
 from config import config
-from scrapper import scrap_dailynews, scrap_mgronline, scrap_matichon, scrap_bkkbiznews, scrap_the_standard
+from scrapper import scrap_dailynews, scrap_mgronline, scrap_matichon, scrap_bkkbiznews, scrap_posttoday, scrap_the_standard, \
+                     scrap_prachachat, scrap_posttoday
 
 scheduler = AsyncIOScheduler()
 
@@ -19,20 +20,30 @@ trigger_configs = config['agency']['mgronline']['scheduler']
 for trigger_config in trigger_configs:
     scheduler.add_job(scrap_mgronline,
                       trigger=CronTrigger(**trigger_config))
-
+#matichon
 trigger_configs = config['agency']['matichon']['scheduler']
 for trigger_config in trigger_configs:
     scheduler.add_job(scrap_matichon,
                       trigger=CronTrigger(**trigger_config))
-
+#bkkbiznews
 trigger_configs = config['agency']['bkkbiznews']['scheduler']
 for trigger_config in trigger_configs:
     scheduler.add_job(scrap_bkkbiznews,
                       trigger=CronTrigger(**trigger_config))
-
+#the_standard
 trigger_configs = config['agency']['the_standard']['scheduler']
 for trigger_config in trigger_configs:
     scheduler.add_job(scrap_the_standard,
+                      trigger=CronTrigger(**trigger_config))
+# # prachachat
+# trigger_configs = config['agency']['prachachat']['scheduler']
+# for trigger_config in trigger_configs:
+#     scheduler.add_job(scrap_prachachat,
+#                       trigger=CronTrigger(**trigger_config))
+#posttoday
+trigger_configs = config['agency']['posttoday']['scheduler']
+for trigger_config in trigger_configs:
+    scheduler.add_job(scrap_posttoday,
                       trigger=CronTrigger(**trigger_config))
 
 scheduler.start()
